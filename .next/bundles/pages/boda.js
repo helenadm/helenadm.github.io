b90af1dcc5afcbd60b3af15889a5281f0c2279f5
Helena Domo <helena@kyso.io>
2017-06-06 20:16:38 +0200

    test

--- .next/bundles/pages/boda.js
+++ .next/bundles/pages/boda.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/boda', function() {
-            var comp = module.exports=webpackJsonp([6],{103:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(0),a=l(n),r=u(95),d=l(r),f=u(18),o=l(f),c=function(){return a.default.createElement("div",null,a.default.createElement(o.default,null),a.default.createElement(d.default,null))};t.default=c},214:function(e,t,u){e.exports=u(103)}},[214]);
+            var comp = module.exports=webpackJsonp([6],{193:function(e,t,u){e.exports=u(98)},98:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(1),a=l(n),r=u(92),d=l(r),f=u(22),o=l(f),c=function(){return a.default.createElement("div",null,a.default.createElement(o.default,null),a.default.createElement(d.default,null))};t.default=c}},[193]);
             return { page: comp.default }
           })
         
---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exportPathMap: function () {
+   return {
+     "/": { pages: "/" },
+     "/index": { pages: "/index" },
+     "/first": { pages: "/first" },
+     "/boda": { pages: "/boda" },
+   }
+ },
+}
